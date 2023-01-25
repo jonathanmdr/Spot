a1db28c8278323aabe8bb76d9ee7097a6182a6f1
Jonathan Henrique <jonathan.mdr@hotmail.com>
2023-01-25 20:15:46 -0300

    Logger processed order use case flow added

--- src/main/java/com/cloud/stream/spot/domain/order/event/OrderCreatedEvent.java
+++ src/main/java/com/cloud/stream/spot/domain/order/event/OrderCreatedEvent.java
@@ -6,6 +6,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+/**
+ * The '@JsonProperty' annotation is a workaround for use correctly the Jackson Serializer/Deserializer
+ * this problem exists only for 'record' type.
+ * However, the '@JsonProperty' is not recommended for use in the domain objects.
+ *
+ * @param orderId
+ * @param customerId
+ * @param value
+ * @param status
+ */
 public record OrderCreatedEvent(
         @JsonProperty("order_id") UUID orderId,
         @JsonProperty("customer_id") UUID customerId,
---
+++ src/main/java/com/cloud/stream/spot/infrastructure/order/listener/OrderProcessedListener.java
@@ -0,0 +1,21 @@
+package com.cloud.stream.spot.infrastructure.order.listener;
+
+import com.cloud.stream.spot.domain.order.event.OrderProcessedEvent;
+import com.cloud.stream.spot.infrastructure.configuration.json.Json;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+public class OrderProcessedListener implements Consumer<OrderProcessedEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderProcessedListener.class);
+
+    @Override
+    public void accept(final OrderProcessedEvent event) {
+        logger.info(Json.writeValueAsString(event));
+    }
+
+}
