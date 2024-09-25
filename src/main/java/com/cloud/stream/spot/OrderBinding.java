4a433798f63141dba06f330728033e450b19737e
Jonathan Henrique Medeiros <jonathan.medeiros@picpay.com>
2024-09-25 01:02:54 -0300

    refactor to use the reactive functions

--- src/main/java/com/cloud/stream/spot/OrderBinding.java
+++ src/main/java/com/cloud/stream/spot/OrderBinding.java
@@ -2,7 +2,8 @@ package com.cloud.stream.spot;
 
 public enum OrderBinding {
 
-    ORDER_CREATED("orderCreatedProducer-out-0");
+    ORDER_CREATED_ONE("orderCreatedProducer-out-0"),
+    ORDER_CREATED_TWO("orderCreatedProducer-out-1");
 
     private final String channel;
 
--- src/main/java/com/cloud/stream/spot/SpotApplication.java
+++ src/main/java/com/cloud/stream/spot/SpotApplication.java
@@ -2,19 +2,30 @@ package com.cloud.stream.spot;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.config.GlobalChannelInterceptor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.math.BigDecimal;
+import java.util.Base64;
+import java.util.Random;
 import java.util.UUID;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -32,29 +43,58 @@ public class SpotApplication {
     }
 
     @Bean
-    public Function<OrderCreatedEvent, OrderProcessedEvent> orderCreatedListener() {
-        return orderCreatedEvent -> {
-            final var order = Order.buildOrderWith(
-                orderCreatedEvent.orderId(),
-                orderCreatedEvent.customerId(),
-                orderCreatedEvent.value(),
-                orderCreatedEvent.status()
-            ).process();
-            return new OrderProcessedEvent(
-                order.getOrderId(),
-                order.getCustomerId(),
-                order.getValue(),
-                order.getStatus()
-            );
+    public Function<Tuple2<Flux<OrderCreatedEvent>, Flux<OrderCreatedEvent>>, Tuple2<Flux<OrderProcessedEvent>, Flux<OrderProcessedEvent>>> orderCreatedListener() {
+        return tuple -> {
+            final var orderProcessedEventFlux = tuple.getT1().mergeWith(tuple.getT2())
+                .map(orderCreatedEvent -> {
+                    final var order = Order.buildOrderWith(
+                        orderCreatedEvent.orderId(),
+                        orderCreatedEvent.customerId(),
+                        orderCreatedEvent.value(),
+                        orderCreatedEvent.status()
+                    ).process();
+                    return new OrderProcessedEvent(
+                        order.getOrderId(),
+                        order.getCustomerId(),
+                        order.getValue(),
+                        order.getStatus()
+                    );
+                });
+            return Tuples.of(orderProcessedEventFlux, orderProcessedEventFlux);
         };
     }
 
     @Bean
-    public Consumer<OrderProcessedEvent> orderProcessedListener() {
+    public Consumer<OrderProcessedEvent> orderProcessedListenerLegacy() {
         return orderProcessedEvent -> {
             final var jsonEvent = Json.writeValueAsString(orderProcessedEvent);
-            log.info(jsonEvent);
+            log.info("Order processed from cluster legacy: {}", jsonEvent);
         };
+    }
+
+    @Bean
+    public Consumer<OrderProcessedEvent> orderProcessedListenerNew() {
+        return orderProcessedEvent -> {
+            final var jsonEvent = Json.writeValueAsString(orderProcessedEvent);
+            log.info("Order processed from cluster new: {}", jsonEvent);
+        };
+    }
+
+    @Component
+    @GlobalChannelInterceptor(
+        patterns = {
+            "*-out-*"
+        }
+    )
+    public static class CustomChannelInterceptor implements ChannelInterceptor {
+        @Override
+        public void afterSendCompletion(@NotNull final Message<?> message, @NotNull final MessageChannel channel, final boolean sent, final Exception ex) {
+            final var jsonMessage = Json.writeValueAsString(message.getPayload());
+            final var cleanedJsonMessage = jsonMessage.replace("\"", "");
+            final var decodedJsonMessageBytes = Base64.getDecoder().decode(cleanedJsonMessage);
+            final var decodedJsonMessage = new String(decodedJsonMessageBytes);
+            log.info("Message sent: {}", decodedJsonMessage);
+        }
     }
 
     @Bean
@@ -67,9 +107,11 @@ public class SpotApplication {
     public static class OrderController {
 
         private final StreamBridge streamBridge;
+        private final Random random;
 
         public OrderController(final StreamBridge streamBridge) {
             this.streamBridge = streamBridge;
+            this.random = new Random();
         }
 
         @PostMapping
@@ -82,7 +124,11 @@ public class SpotApplication {
                 newOrder.getValue(),
                 newOrder.getStatus()
             );
-            this.streamBridge.send(OrderBinding.ORDER_CREATED.channel(), orderCreatedEvent);
+
+            final var randomBinding = this.random.nextInt(OrderBinding.values().length);
+            final var randomChannel = OrderBinding.values()[randomBinding].channel();
+            log.info("Sending order to channel: {}", randomChannel);
+            this.streamBridge.send(randomChannel, orderCreatedEvent);
         }
 
         public record OrderInput(
